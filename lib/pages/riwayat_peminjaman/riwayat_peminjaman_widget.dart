f6d607410993a081d7468aa79bb4f6d0bce6e540
Gerry Bukit <ifs21055@students.del.ac.id>
2025-03-03 00:57:24 +0700

    fix riwayat peminjaman scrollview

--- lib/pages/riwayat_peminjaman/riwayat_peminjaman_widget.dart
+++ lib/pages/riwayat_peminjaman/riwayat_peminjaman_widget.dart
@@ -1,7 +1,6 @@
 import 'package:library_application/config.dart';
 
 import '/components/bottom_bar_profile_widget.dart';
-import '/flutter_flow/flutter_flow_button_tabbar.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -20,8 +19,7 @@ class RiwayatPeminjamanWidget extends StatefulWidget {
       _RiwayatPeminjamanWidgetState();
 }
 
-class _RiwayatPeminjamanWidgetState extends State<RiwayatPeminjamanWidget>
-    with TickerProviderStateMixin {
+class _RiwayatPeminjamanWidgetState extends State<RiwayatPeminjamanWidget> {
   late RiwayatPeminjamanModel _model;
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -34,12 +32,6 @@ class _RiwayatPeminjamanWidgetState extends State<RiwayatPeminjamanWidget>
     _model = createModel(context, () => RiwayatPeminjamanModel());
     _model.textController ??= TextEditingController();
     _model.textFieldFocusNode ??= FocusNode();
-
-    _model.tabBarController = TabController(
-      vsync: this,
-      length: 1,
-      initialIndex: 0,
-    )..addListener(() => safeSetState(() {}));
 
     fetchRiwayatPeminjaman();
   }
@@ -106,6 +98,10 @@ class _RiwayatPeminjamanWidgetState extends State<RiwayatPeminjamanWidget>
 
   @override
   Widget build(BuildContext context) {
+    // Dapatkan tinggi bottom bar untuk padding
+    final bottomBarHeight = MediaQuery.of(context).padding.bottom +
+        70.0; // Perkiraan tinggi bottom bar
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -147,171 +143,107 @@ class _RiwayatPeminjamanWidgetState extends State<RiwayatPeminjamanWidget>
             Column(
               mainAxisSize: MainAxisSize.max,
               children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color:
-                              FlutterFlowTheme.of(context).secondaryBackground,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              15.0, 20.0, 15.0, 0.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              SizedBox(
-                                width: double.infinity,
-                                child: TextFormField(
-                                  controller: _model.textController,
-                                  focusNode: _model.textFieldFocusNode,
-                                  autofocus: false,
-                                  obscureText: false,
-                                  decoration: InputDecoration(
-                                    isDense: true,
-                                    labelStyle: FlutterFlowTheme.of(context)
-                                        .labelMedium
-                                        .override(
-                                          fontFamily: 'Inter',
-                                          letterSpacing: 0.0,
-                                        ),
-                                    hintText: 'Cari berdasarkan judul ',
-                                    hintStyle: FlutterFlowTheme.of(context)
-                                        .labelMedium
-                                        .override(
-                                          fontFamily: 'Inter',
-                                          letterSpacing: 0.0,
-                                        ),
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                        color: Color(0xFF222222),
-                                        width: 1.0,
-                                      ),
-                                      borderRadius: BorderRadius.circular(24.0),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                        color: Colors.black,
-                                        width: 1.0,
-                                      ),
-                                      borderRadius: BorderRadius.circular(24.0),
-                                    ),
-                                    errorBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color:
-                                            FlutterFlowTheme.of(context).error,
-                                        width: 1.0,
-                                      ),
-                                      borderRadius: BorderRadius.circular(24.0),
-                                    ),
-                                    focusedErrorBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color:
-                                            FlutterFlowTheme.of(context).error,
-                                        width: 1.0,
-                                      ),
-                                      borderRadius: BorderRadius.circular(24.0),
-                                    ),
-                                    filled: true,
-                                    fillColor: FlutterFlowTheme.of(context)
-                                        .secondaryBackground,
-                                    prefixIcon: const Icon(
-                                      Icons.search_rounded,
-                                    ),
-                                  ),
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyMedium
-                                      .override(
-                                        fontFamily: 'Inter',
-                                        letterSpacing: 0.0,
-                                      ),
-                                  cursorColor:
-                                      FlutterFlowTheme.of(context).primaryText,
-                                  validator: _model.textControllerValidator
-                                      .asValidator(context),
+                // Search field
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(
+                      15.0, 20.0, 15.0, 16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextFormField(
+                      controller: _model.textController,
+                      focusNode: _model.textFieldFocusNode,
+                      autofocus: false,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        labelStyle:
+                            FlutterFlowTheme.of(context).labelMedium.override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
                                 ),
-                              ),
-                            ],
+                        hintText: 'Cari berdasarkan judul ',
+                        hintStyle:
+                            FlutterFlowTheme.of(context).labelMedium.override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Color(0xFF222222),
+                            width: 1.0,
                           ),
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).error,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).error,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                        filled: true,
+                        fillColor:
+                            FlutterFlowTheme.of(context).secondaryBackground,
+                        prefixIcon: const Icon(
+                          Icons.search_rounded,
                         ),
                       ),
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Inter',
+                            letterSpacing: 0.0,
+                          ),
+                      cursorColor: FlutterFlowTheme.of(context).primaryText,
+                      validator:
+                          _model.textControllerValidator.asValidator(context),
                     ),
-                  ],
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: const Alignment(-1.0, 0),
-                        child: FlutterFlowButtonTabBar(
-                          useToggleButtonStyle: false,
-                          isScrollable: true,
-                          labelStyle:
-                              FlutterFlowTheme.of(context).titleMedium.override(
-                                    fontFamily: 'Inter Tight',
-                                    letterSpacing: 0.0,
-                                  ),
-                          unselectedLabelStyle: const TextStyle(),
-                          labelColor: Colors.white,
-                          unselectedLabelColor:
-                              FlutterFlowTheme.of(context).secondaryText,
-                          backgroundColor: const Color(0xFF2679C2),
-                          borderColor: const Color(0xFF2679C2),
-                          borderWidth: 2.0,
-                          borderRadius: 12.0,
-                          elevation: 0.0,
-                          labelPadding: const EdgeInsetsDirectional.fromSTEB(
-                              16.0, 0.0, 16.0, 0.0),
-                          buttonMargin: const EdgeInsetsDirectional.fromSTEB(
-                              0.0, 12.0, 16.0, 0.0),
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              16.0, 0.0, 16.0, 0.0),
-                          tabs: const [
-                            Tab(
-                              text: 'Riwayat',
-                            ),
-                          ],
-                          controller: _model.tabBarController,
-                          onTap: (i) async {
-                            [() async {}][i]();
-                          },
-                        ),
-                      ),
-                      Expanded(
-                        child: TabBarView(
-                          controller: _model.tabBarController,
-                          children: [
-                            isLoading
-                                ? const Center(
-                                    child: CircularProgressIndicator())
-                                : riwayatPeminjaman.isEmpty
-                                    ? const Center(
-                                        child: Text(
-                                            'Tidak ada riwayat peminjaman selesai'))
-                                    : ListView.builder(
-                                        padding: EdgeInsets.zero,
-                                        itemCount: riwayatPeminjaman.length,
-                                        itemBuilder: (context, index) {
-                                          final riwayat =
-                                              riwayatPeminjaman[index];
-                                          return buildRiwayatCard(riwayat);
-                                        },
-                                      ),
-                          ],
-                        ),
-                      ),
-                    ],
                   ),
+                ),
+
+                // List view with bottom padding
+                Expanded(
+                  child: isLoading
+                      ? const Center(child: CircularProgressIndicator())
+                      : riwayatPeminjaman.isEmpty
+                          ? const Center(
+                              child:
+                                  Text('Tidak ada riwayat peminjaman selesai'))
+                          : Padding(
+                              padding: EdgeInsets.only(bottom: bottomBarHeight),
+                              child: ListView.builder(
+                                padding: EdgeInsets.zero,
+                                itemCount: riwayatPeminjaman.length,
+                                itemBuilder: (context, index) {
+                                  final riwayat = riwayatPeminjaman[index];
+                                  return buildRiwayatCard(riwayat);
+                                },
+                              ),
+                            ),
                 ),
               ],
             ),
-            wrapWithModel(
-              model: _model.bottomBarProfileModel,
-              updateCallback: () => safeSetState(() {}),
-              child: const BottomBarProfileWidget(),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: wrapWithModel(
+                model: _model.bottomBarProfileModel,
+                updateCallback: () => safeSetState(() {}),
+                child: const BottomBarProfileWidget(),
+              ),
             ),
           ],
         ),
