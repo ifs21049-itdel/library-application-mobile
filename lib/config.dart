4811f34dd89f838eb1611833b0b94fbc8cf54826
Gerry Bukit <ifs21055@students.del.ac.id>
2025-03-17 09:14:44 +0700

    menambahkan notifikasi ketika ada buku baru dan status peminjaman buku

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
 // const String apiUrl = 'http://10.0.2.2:3000';
-// const String apiUrl = 'http://172.27.80.38:3000';
-const String apiUrl = 'http://192.168.35.117:3000';
+const String apiUrl = 'http://172.27.67.85:3000';
+// const String apiUrl = 'http://192.168.35.117:3000';
